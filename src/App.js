b9bc42bf44ffaa96464f1ed2bbfb31edd8cd8485
lakshay91dutt <lakshay91dutt@gmail.com>
2019-02-07 10:46:41 +0530

    changed directory structure and added key to custom components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App1 from './subModuleAPP';
+import App1 from './customComponents/registerEmployee';
 class App extends Component {
     constructor(props) {
         super(props);
---
+++ src/customComponents/registerEmployee.js
@@ -0,0 +1,213 @@
+/*
+    Candidate
+            Registration 
+*/
+
+import React, { Component } from "react";
+import './../multiMedia/css/normalize.css';
+import './../multiMedia/css/style.css';
+import './../multiMedia/css/grid.css';
+import './../multiMedia/css/queries.css';
+
+//React module - To use predefined form elements
+import AllELements from '/home/lakshay/Desktop/WorkSpace/admin-console/src/customComponents/formElements'
+
+//Node module - to write Form data into local JSON file
+import ProcessRecords from '/home/lakshay/Desktop/WorkSpace/admin-console/src/dataFiles/dataFileOperations';
+class App1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeData: {
+        firstName: "Lakshay",
+        lastName: "",
+        emailId: "",
+        mobile: "",
+        dob: "",
+        address: "",
+        designation: "",
+        department: "",
+        ctc: "",
+        status: "",
+        fileUrl: ""
+      },
+      formElementProps: [
+        {
+          labelName: "Applicant ID",
+          id: "applicantId",
+          elementType: "input",
+          type: "text",
+          placeholder: "Unique Registration Id",
+
+          handlerInputChange: this.handleInputChange,
+        },
+        {
+          labelName: "First Name",
+          id: "firstName",
+          elementType: "input",
+          type: "text",
+          placeholder: "First Name",
+
+          handlerInputChange: this.handleInputChange,
+          handlerSubmit: ""
+        },
+        {
+          labelName: "Last Name",
+          id: "lastName",
+          elementType: "input",
+          type: "text",
+          placeholder: "Last Name",
+
+          handlerInputChange: this.handleInputChange
+        },
+        {
+          labelName: "Email ID",
+          id: "emailId",
+          elementType: "input",
+          type: "email",
+          placeholder: "Valid Email Id",
+          handlerInputChange: this.handleInputChange
+        },
+        {
+          labelName: "Mobile Number",
+          id: "mobile",
+          elementType: "input",
+          type: "text",
+          placeholder: "Primary Contact Number",
+
+          handlerInputChange: this.handleInputChange,
+        },
+        {
+          labelName: "Date Of Birth",
+          id: "dob",
+          elementType: "input",
+          type: "date",
+          handlerInputChange: this.handleInputChange,
+        },
+        {
+          labelName: "Address",
+          id: "address",
+          elementType: "textarea",
+          type: "text",
+          placeholder: "Permanent Address",
+          handlerInputChange: this.handleInputChange,
+        },
+        {
+          labelName: "Resume Upload",
+          id: "file",
+          elementType: "input",
+          type: "file",
+          accept: ".pdf",
+          handlerInputChange: this.handleInputChange,
+        },
+        {
+          labelName: "Department",
+          id: "department",
+          elementType: "dropdown",
+          handlerInputChange: this.handleInputChange,
+          select: [
+            { optName: "Android", optValue: "android" },
+            { optName: "IOS", optValue: "ios" },
+            { optName: "Web Development", optValue: "web-dev" },
+            { optName: "DevOps", optValue: "devops" }
+          ],
+        }
+      ]
+    };
+  }
+
+  // Input Handling methods ------- START
+  handleSubmit = event => {
+    event.preventDefault();
+
+    document.write("<p>" + JSON.stringify(this.state) + "</p>");
+
+    ProcessRecords();
+  };
+
+  handleInputChange = event => {
+    event.preventDefault();
+
+    let tempValue;
+    console.log(this.state.employeeData.firstName);
+    if (event.target.name === "fileUrl") {
+      tempValue = event.target.files[0];
+    } else {
+      tempValue = event.target.value;
+    }
+    this.setState(this.state, () => {
+      return { employeeData: { firstName: tempValue } };
+    });
+  };
+  // Input Handling methods ------- END
+
+  render() {
+    return (
+      <div className="App">
+        <div className="row">
+          <h1>Candidate Registration</h1>
+        </div>
+
+        <section className="section-form">
+          <form method="post" className="contact-form">
+            {this.state.formElementProps.map(formElementProps => {
+              return (
+                <AllELements
+                  key={formElementProps.id}
+                  labelName={formElementProps.labelName}
+                  id={formElementProps.id}
+                  elementType={formElementProps.elementType}
+                  type={formElementProps.type}
+                  placeholder={formElementProps.placeholder}
+                  handlerInputChange={formElementProps.handlerInputChange}
+                  value={formElementProps.value}
+                />
+              );
+            })}
+            {/* -------------Marital Status----------------- */}
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label>Status</label>
+              </div>
+              <div className="col span-2-of-3">
+                <label>Married: </label>
+                <input
+                  type="checkbox"
+                  name="status"
+                  value="married"
+                  onChange={this.handleInputChange}
+                  defaultChecked
+                />
+                <label>Single: </label>
+                <input
+                  type="checkbox"
+                  name="status"
+                  value="Single"
+                  onChange={this.handleInputChange}
+                />
+              </div>
+            </div>
+            {/* -------------Reusable ---- Buttons----------------- */}
+            <AllELements
+              elementType="button"
+              type="submit"
+              value="Submit"
+              handlerSubmit={this.handleSubmit}
+            />
+
+            <AllELements
+              elementType="button"
+              type="reset"
+              value="Reset"
+              handlerSubmit={this.handleSubmit}
+            />
+          </form>
+        </section>
+
+        {/* --------------------------------End of form-------------------------------- */}
+      </div>
+    );
+  }
+}
+
+export default App1;
